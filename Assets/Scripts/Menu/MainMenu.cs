ac366c2f0abae5857cdac4f03f41073e6638dc9e
Páll Márton <palmarbg@gmail.com>
2026-06-12 18:48:51 +0200

    Háttérzene és hangok

--- Assets/Scripts/Menu/MainMenu.cs
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -1,18 +1,35 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField]
+    private AudioSource _audioSource;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        var buttons = GetComponentsInChildren<Button>();
+        foreach (var button in buttons)
+        {
+            button.onClick.AddListener(PlayClickSound);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
 
+    }
+
+    private void OnDestroy()
+    {
+        var buttons = GetComponentsInChildren<Button>();
+        foreach (var button in buttons)
+        {
+            button.onClick.RemoveListener(PlayClickSound);
+        }
     }
 
     public void Play()
@@ -23,5 +40,10 @@ public class MainMenu : MonoBehaviour
     public void Exit()
     {
         Application.Quit();
+    }
+
+    private void PlayClickSound()
+    {
+        _audioSource.PlayOneShot(_audioSource.clip);
     }
 }
